86d4d17222f675e5485a968cb4184a2a3fd1d6d0
Jason A. Cox <jason@jasonacox.com>
2023-06-14 21:47:10 -0700

    Add chat function

--- llm/nanoGPT/chat.py
+++ llm/nanoGPT/chat.py
@@ -18,7 +18,7 @@ out_dir = 'out' # ignored if init_from is not 'resume'
 start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
 num_samples = 1 # number of samples to draw
 max_new_tokens = 500 # number of tokens generated in each sample
-temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
+temperature = 1.0 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
 seed = 1337
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
